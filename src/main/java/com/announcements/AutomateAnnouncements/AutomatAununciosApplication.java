575893250b961ef145e59cade0e0f69d34f19f90
Matheu2b2t <johnatan.ruales@campusucc.edu.co>
2025-11-11 02:42:53 -0500

    feat: clase principal de la aplicación (Spring Boot)

---
+++ src/main/java/com/announcements/AutomateAnnouncements/AutomatAununciosApplication.java
@@ -0,0 +1,13 @@
+package com.announcements.AutomateAnnouncements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatAununciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatAununciosApplication.class, args);
+	}
+
+}
