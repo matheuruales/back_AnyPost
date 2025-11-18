8afc339033705b1005434848bafbe9e3430a497b
Matheu2b2t <johnatan.ruales@campusucc.edu.co>
2025-11-18 14:28:30 -0500

    solucion blob storage

--- src/main/java/com/announcements/AutomateAnnouncements/config/SecurityConfig.java
+++ src/main/java/com/announcements/AutomateAnnouncements/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
                 .requestMatchers("/v3/api-docs/**").permitAll()
                 .requestMatchers("/swagger-ui/**").permitAll()
                 .requestMatchers("/swagger-ui.html").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/videos/stream").permitAll()
                 .requestMatchers(HttpMethod.GET, "/").permitAll()
                 .anyRequest().authenticated()
             )
